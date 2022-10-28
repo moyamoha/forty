c2d29cef298b837434c2ab7451beabc03a016cae
Yahya Salimi <yahya.salimi@eduhouse.fi>
2022-10-28 21:34:46 +0300

    create package-all script

--- 40days/package-all.js
+++ 40days/package-all.js
@@ -1,9 +1,10 @@
 const { exec } = require("child_process");
 
 // Run this only when you make changes to frontend too
+// This is for packaging only
 const sh = "C:\\Program Files\\git\\bin\\bash.exe";
 exec(
-  "cd ../frontend && npm run build && cp -r build/* ../40days/html/ && cd ../40days && npm run make",
+  "cd ../frontend && npm run build && cp -r build/* ../40days/html/ && cd ../40days && npm run package",
   { shell: sh },
   (e, st, se) => {
     if (e) {
